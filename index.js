d50e4dbb5ef5c5563c75616bc3bae2eec2f39735
Trunday <musqa25@hotmail.com>
2021-03-24 13:49:14 +0300

    Day 3

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const app = express()
 
-app.get('/',function(req , res){
-    return res.send('Selam sss')
+app.get('/', function (req, res) {
+  return res.send('Selam sss')
 })
 
-app.listen(3000,function(){
-    console.log("Sistem 3000 de çalışıyor.");
+app.listen(3000, function () {
+  console.log('Sistem 3000 de çalışıyor.')
 })
