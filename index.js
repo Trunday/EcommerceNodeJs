604ffe97e4dc83c5c894b8f2e779435b3f1835a6
Trunday <musqa25@hotmail.com>
2021-03-24 13:17:18 +0300

    Day2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/',function(req , res){
-    return res.send('Selam')
+    return res.send('Selam sss')
 })
 
 app.listen(3000,function(){
